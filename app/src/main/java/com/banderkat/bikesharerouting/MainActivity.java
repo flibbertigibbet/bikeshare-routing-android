d73eeaa807d6aa49a600bddef3afb003f9f86436
Kathryn Killebrew <kkillebrew@azavea.com>
2017-03-30 16:34:31 -0400

    Set up map
    Upgrade Mapbox SDK version. Show a map on login button click (does not log in to anything).

--- app/src/main/java/com/banderkat/bikesharerouting/MainActivity.java
+++ app/src/main/java/com/banderkat/bikesharerouting/MainActivity.java
@@ -2,12 +2,89 @@ package com.banderkat.bikesharerouting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_api_key));
+
+        MapboxMapOptions options = new MapboxMapOptions()
+                .styleUrl(Style.OUTDOORS)
+                .camera(new CameraPosition.Builder()
+                        .target(new LatLng(43.7383, 7.4094))
+                        .zoom(12)
+                        .build());
+
+        // create map
+        mapView = new MapView(this, options);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+
+                // Customize map with markers, polylines, etc.
+                Log.d("MainActivity", "Map is ready!");
+
+            }
+        });
+
+        setContentView(mapView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
